c620c2e5b567557d6a8e1b701eb8ebbf9e9d1f03
ArtemBereznii <artem.berezniy8@gmail.com>
2026-04-16 17:40:51 +0300

    feat: create placeholder Furniture class

---
+++ PI.DAL/Entities/Products/Furniture/Furniture.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PI.DAL.Entities.Products.Furniture
+{
+    internal class Furniture : BaseEntity
+    {
+
+    }
+}

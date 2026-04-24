3583c0254e31d3b570f07a23d169b7f92a2258fe
temandro <9128753@stud.kai.edu.ua>
2026-04-24 10:15:07 +0300

    feat(DAL): implement Order entity

---
+++ PI.DAL/Entities/Orders/Order.cs
@@ -0,0 +1,29 @@
+using PI.DAL.Entities.Identity;
+using PI.DAL.Enums;
+
+namespace PI.DAL.Entities.Orders;
+
+public class Order : BaseEntity
+{
+    public Guid UserId { get; private set; }
+    public OrderStatus Status { get; private set; }
+    public decimal TotalAmount { get; private set; }
+    public User User {get; private set; } = null!;
+        
+    protected Order() { } 
+
+    private Order(Guid userId, decimal totalAmount, OrderStatus status = OrderStatus.New)
+    {
+        UserId = userId;
+        TotalAmount = totalAmount;
+        Status = status;
+    }
+
+    public static Order Create(Guid userId, decimal totalAmount, OrderStatus status = OrderStatus.New)
+    {
+        if (totalAmount < 0)
+            throw new ArgumentException("Total amount cannot be negative.", nameof(totalAmount));
+
+        return new Order(userId, totalAmount, status);
+    }
+}
---
+++ PI.DAL/Enums/OrderStatus.cs
@@ -0,0 +1,9 @@
+namespace PI.DAL.Enums;
+
+public enum OrderStatus 
+{
+    New = 1,
+    Paid = 2,
+    Delivered = 3,
+    Cancelled = 4
+}

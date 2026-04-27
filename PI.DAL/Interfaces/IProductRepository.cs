8ae6737266ed5cd68eded5c19057a252f0e67741
ArtemBereznii <artem.berezniy8@gmail.com>
2026-04-27 23:13:36 +0300

    feat(DAL): create IProductRepository interface

---
+++ PI.DAL/Interfaces/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using PI.DAL.Entities.Catalog;
+using PI.DAL.Models;
+
+namespace PI.DAL.Interfaces;
+
+public interface IProductRepository : IBaseRepository<Product>
+{
+    Task<(IEnumerable<Product> Items, int TotalCount)> GetFilteredPagedAsync(ProductFilterParams filter, CancellationToken cancellationToken = default);
+    Task<Product?> GetWithDetailsAsync(Guid id, CancellationToken cancellationToken = default);
+}
